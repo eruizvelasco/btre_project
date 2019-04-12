f90c5eb0eed96c969b58fe365a304ffef8dc3451
Enrique Ruiz-Velasco <enrique@Chamuel-16.frontierlocal.net>
2019-04-12 16:10:02 -0500

    Added dbusre/dbpassword variables

--- btre/settings.py
+++ btre/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from btre.passwords import email_user, email_password
+from btre.passwords import email_user, email_password, db_user, db_password
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
